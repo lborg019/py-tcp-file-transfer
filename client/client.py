86b01b0ced4f1182d011fef6043c61dbdf3ff95c
Lukas Borges <lborges@allnowcorp.com>
2017-01-21 22:09:04 -0500

    first push

---
+++ client/client.py
@@ -0,0 +1,69 @@
+##########################################################################
+"""  TCPClient.py                                     
+Use the better name for this module:   MakeUpperCaseClientUsingTCP   
+  
+[STUDENTS FILL IN THE ITEMS BELOW]  
+  STUDENT NAME                                 
+  COURSE NAME and SEMESTER                    
+  DATE                                         
+  This module will <blah, blah, blah>              
+"""
+
+from socket import *
+import os
+# STUDENTS - replace your server machine's name 
+serverName = "localhost"
+
+# STUDENTS - you should randomize your port number.         
+# This port number in practice is often a "Well Known Number"  
+serverPort = 12000
+
+# create TCP socket on client to use for connecting to remote
+# server.  Indicate the server's remote listening port
+# Error in textbook?   socket(socket.AF_INET, socket.SOCK_STREAM)  Amer 4-2013 
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# open the TCP connection
+clientSocket.connect((serverName,serverPort))
+
+print ("Available commands:\n"+
+       "help: list available commands\n"+
+       "ls-local: list local files\n"+
+       "ls-remote: list remote files\n"+
+       "get 'filename': download remote file to local\n"+
+       "put 'filename': upload local file to remote\n"+
+       "/quit or /exit: terminates program")
+path = ("./")
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+while 1:
+  # interactively get user's line to be converted to upper case
+  # authors' use of raw_input changed to input for Python 3  Amer 4-2013
+  sentence = raw_input('command:') # input for windows / raw_input for unix
+
+  if(sentence=="ls-local"):
+    print("local files:")
+    for f in files:
+      print(f)
+
+  # send the user's line over the TCP connection
+  # No need to specify server name, port
+  # sentence casted to bytes for Python 3  Amer 4-2013
+
+  # modified to run on windows:
+  # clientSocket.send(sentence)
+  clientSocket.sendto(sentence.encode(),(serverName, serverPort))
+
+  #output to console what is sent to the server
+  print ("Sent to Make Upper Case Server: ", sentence)
+
+  # get user's line back from server having been modified by the server
+  modifiedSentence = clientSocket.recv(1024)
+
+  # output the modified user's line 
+  # print ("Received from Make Upper Case Server: ", modifiedSentence)
+  print(modifiedSentence)
+
+# close the TCP connection
+clientSocket.close()
+
---
+++ server/server.py
@@ -0,0 +1,79 @@
+##########################################################################
+""" TCPServer.py                                     
+Use the better name for this module:   MakeUpperCaseServerUsingTCP   
+  
+[STUDENTS FILL IN THE ITEMS BELOW]  
+  STUDENT NAME                                 
+  COURSE NAME and SEMESTER                    
+  DATE                                         
+  This module will <blah, blah, blah>              
+"""
+
+from socket import *
+import os
+
+_help = "help"
+_lsremote = "ls-remote"
+_get = "get "
+_put = "put "
+_quit = "/quit"
+_exit = "/exit"
+
+path = ("./")
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+fileList = []
+remoteList = "remote files:"
+
+# STUDENTS: randomize this port number (use same one that client uses!)
+serverPort = 12000
+
+# create TCP welcoming socket
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(("",serverPort))
+
+# server begins listening for incoming TCP requests
+serverSocket.listen(1)
+
+# output to console that server is listening 
+print ("The Make Upper Case Server running over TCP is ready to receive ... ")
+
+# server waits for incoming requests; new socket created on return
+connectionSocket, addr = serverSocket.accept()
+
+while 1:     
+    # read a sentence of bytes from socket sent by the client
+    sentence = connectionSocket.recv(1024)
+
+    # output to console the sentence received from the client 
+    print ("Received From Client: ", sentence)
+
+    # check what message was sent, process accordingly
+    if sentence == _help.lower():
+      print ("received help");
+    elif sentence == _lsremote.lower():
+      print("remote files:")
+      for f in files:
+        remoteList += ("\n"+f)
+        # fileList.append(f)
+        # print(f)
+      connectionSocket.send(remoteList)
+      print ("received ls-remote");
+    elif sentence == _get.lower():
+      print ("received a get");
+    else:
+      print ("received some bs command");
+	 
+    # convert the sentence to upper case
+    capitalizedSentence = sentence.upper()
+	 
+    # send back modified sentence over the TCP connection
+    connectionSocket.send(capitalizedSentence)
+
+    # output to console the sentence sent back to the client 
+    print ("Sent back to Client: ", capitalizedSentence)
+
+# close the TCP connection; the welcoming socket continues
+connectionSocket.close()
+	 
+##########################################################################
+
